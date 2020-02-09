58182eb0c25d490946cfa83d33fa6b588a09d50a
arfsz <58475184+arfsz@users.noreply.github.com>
2020-02-09 17:26:33 +0300

    Delete Adventure game.py

--- Python projects (Beginner)/Adventure game.py
+++
@@ -1,5 +0,0 @@
-#This is going to be a simple choose your adventure game that uses text
-#Since this is a simple game we will use alot of if statements.
-#In the future I will use a gui or pygame to visualize the game and make it better
-#Side note: The documentation for the game (Story, settings, theme, etc.) will be on a seperate document
-
